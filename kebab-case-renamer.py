4b919395f0522ee5fc91719b7c752dc209b5886d
Chris Phu <chrisnguyenphu@gmail.com>
2025-04-26 18:59:09 -0500

    Added '-r' '--recur' flag to recursively run in descendant directories as well.

--- kebab-case-renamer.py
+++ kebab-case-renamer.py
@@ -3,11 +3,21 @@ import os
 from pathlib import Path
 
 
+rename_count = 0
+
+
 def add_and_parse_args(parser):
     parser.add_argument(
-        'directory_path',
+        "directory_path",
         type=Path,
-        help="Path of the directory hosting the files and directories to be renamed.")
+        help="Path of the directory hosting the files and directories to be renamed."
+    )
+    parser.add_argument(
+        "-r",
+        "--recur",
+        action="store_true",
+        help="If renaming should be recursive and affect all descendants instead of only direct children."
+    )
     args = parser.parse_args()
     return args
 
@@ -29,7 +39,7 @@ def query_yes_no(question):
         raise Exception("Answer must be a string.")
 
 
-def snakecase_rename(path):
+def snakecase_rename(path, depth):
     current_name = os.path.basename(path)
     new_name = current_name.lower().replace(' ', '-').replace('_', '-')
     if new_name == current_name:
@@ -37,29 +47,32 @@ def snakecase_rename(path):
     current_path = path.absolute()
     new_path = os.path.join(current_path.parent, new_name)
     os.rename(current_path, new_path)
-    print("'" + str(current_path) + "' renamed to '" + str(new_path) + "'")
+    print("Depth " + str(depth) + ": '" + str(current_path) + "' renamed to '" + str(new_path) + "'")
     return 1
 
 
-def rename_children(path):
-    rename_count = 0
+def rename_children(recur, path, depth):
+    # rename_count = 0
+    global rename_count
     for path_child_i in path.iterdir():
-        rename_count += snakecase_rename(path_child_i)
+        if recur and path_child_i.is_dir():
+            rename_children(True, path_child_i, depth + 1)
+        rename_count += snakecase_rename(path_child_i, depth)
         # TODO: Add 'rename files only' and 'rename directories only' option.
         # if path_child_i.is_file():
         #     snakecase_rename(path_child_i)
         #     continue
-    print("\nDone. " + str(rename_count) + " files/directories renamed in '" + str(path) + "'.")
+    if depth == 0:
+        print("\nDone. " + str(rename_count) + " files/directories renamed.")
 
 
 def main():
     parser = argparse.ArgumentParser()
     args = add_and_parse_args(parser)
-    directory_path = args.directory_path
-    verify_path_is_a_directory(directory_path)
+    verify_path_is_a_directory(args.directory_path)
     query_yes_no("Are you sure you want to rename files and directories in the '"
-                 + str(directory_path) + "' directory? THIS CANNOT BE UNDONE.")
-    rename_children(directory_path)
+                 + str(args.directory_path) + "' directory? THIS CANNOT BE UNDONE.")
+    rename_children(args.recur, args.directory_path, 0)
 
 
 if __name__ == '__main__':
